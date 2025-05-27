10381670cedb1d61e13ff921a100b1ce1db18677
Tom <t910935@gmail.com>
2025-05-27 14:15:37 +0200

    Spread and Rest Operators

--- src/App.jsx
+++ src/App.jsx
@@ -1,115 +1,79 @@
-import * as React from 'react';
-/**/
+import * as React from "react";
 
 const App = () => {
-const stories = [
-  {
-    title: 'React',
-    url: 'https://react.dev/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-]
+  const stories = [
+    {
+      title: "React",
+      url: "https://react.dev/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
 
-// A
-const handleSearch = (event) => {
-// D
-console.log(event.target.value);
-}
+  const [searchTerm, setSearchTerm] = React.useState("React");
 
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
 
+  const searchedStories = stories.filter((story) =>
+    story.title.toLowerCase().includes(searchTerm.toLowerCase()),
+  );
 
-  return(
- <div>
+  return (
+    <div>
       <h1>my Hacker stories</h1>
-        
-        {/* // B */}
-        <Search onSearch={handleSearch} />
 
-    
+      <Search search={searchTerm} onSearch={handleSearch} />
+
       <hr />
-        <List list={stories}/>
+      <List list={searchedStories} />
     </div>
+  );
+};
 
-    )
-  
-   
-}
+/*component*/
+const Search = ({ search, onSearch }) => (
+  <div>
+    <label htmlFor="search">Search: </label>
 
+    <input id="search" 
+      type="text" 
+      value={search} 
+      onChange={onSearch} />
+  </div>
+);
 
- 
-/* state = value  | state updater function = setValue |
-React.useState('') = initial state
-  */
- const Search = (props)=> {
- const [searchTerm, setSearchTerm] = React.useState('');
-
-   const handleChange = (event)=>{
-    setSearchTerm(event.target.value)
-
-    // C
-    props.onSearch(event)
-   }
-
-
-  return(
-     <div>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={handleChange} />
+// keep Variation 1: Spread and Rest Operators
+// Final Step
+const List = ({list}) => (
+  <ul>
+    {list.map((item) => (
+      <Item key={item.objectID} item={item} />
+    ))}
+  </ul>
+);
 
 
-       <p>
-          Searching for <strong>{searchTerm}</strong>.
-       </p>
-      </div>
-
-    )
-
- }
-  
-    
-  
-
-/**/
- const List = (props) =>(
-  
-    <ul>
-      {props.list.map((item)=> (
-        <Item key={item.objectID} item={item}/>
-        
-      ))}
-    </ul>
-  
-)
-
-
- /*            */
-const Item = (props)=>(
+const Item = ({ item }) => (
   <li>
-    
-      <span>
-        <a href={props.item.url}>{props.item.title}</a>
-      </span>
-      <span>{props.item.author}</span>
-      <span>{props.item.num_comments}</span>
-      <span>{props.item.points}</span> 
-    
+    <span>
+      <a href={item.url}>{item.title}</a>
+    </span>
+    <span>{item.author} </span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
   </li>
-)
-
-
-
-
-
+);
 
 export default App;
