0857a1ec79730957dcded0fef8f9b779b1f033c6
Tom <t910935@gmail.com>
2025-06-05 14:59:57 +0200

    custom hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,61 @@
-import * as React from "react";
+import * as React from 'react';
+
+
+
+// use as prefix  and return array add value and key 
+const useStorageState = (key,initialSate) => {
+  const [value, setValue] = React.useState(
+    localStorage.getItem(key) || initialSate
+  );
+  React.useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [value,key]);
+
+  return [value,setValue]
+};
+
 
 const App = () => {
   const stories = [
     {
-      title: "React",
-      url: "https://react.dev/",
-      author: "Jordan Walke",
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
       num_comments: 3,
       points: 4,
       objectID: 0,
     },
     {
-      title: "Redux",
-      url: "https://redux.js.org/",
-      author: "Dan Abramov, Andrew Clark",
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
       num_comments: 2,
       points: 5,
       objectID: 1,
     },
   ];
 
-  const [searchTerm, setSearchTerm] = React.useState("React");
+ const [searchTerm, setSearchTerm] = useStorageState(
+  'search',
+  'React'
+  );
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
   };
 
   const searchedStories = stories.filter((story) =>
-    story.title.toLowerCase().includes(searchTerm.toLowerCase()),
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
   return (
     <div>
-      <h1>my Hacker stories</h1>
+      <h1>My Hacker Stories</h1>
 
       <Search search={searchTerm} onSearch={handleSearch} />
 
       <hr />
+
       <List list={searchedStories} />
     </div>
   );
@@ -44,19 +63,21 @@ const App = () => {
 
 /*component*/
 const Search = ({ search, onSearch }) => (
-  <div>
+  <>
     <label htmlFor="search">Search: </label>
-
-    <input id="search" 
-      type="text" 
-      value={search} 
-      onChange={onSearch} />
-  </div>
+    <input
+      id="search"
+      type="text"
+      value={search}
+      onChange={onSearch}
+    />
+  </>
 );
+
 
 // keep Variation 1: Spread and Rest Operators
 // Final Step
-const List = ({list}) => (
+const List = ({ list }) => (
   <ul>
     {list.map((item) => (
       <Item key={item.objectID} item={item} />
@@ -64,13 +85,12 @@ const List = ({list}) => (
   </ul>
 );
 
-
 const Item = ({ item }) => (
   <li>
     <span>
       <a href={item.url}>{item.title}</a>
     </span>
-    <span>{item.author} </span>
+    <span>{item.author}</span>
     <span>{item.num_comments}</span>
     <span>{item.points}</span>
   </li>
